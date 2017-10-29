b8b6b149d645b41ed9ed71f9cc47910e896b185f
Srgy Surkv <sss1024@gmail.com>
2017-10-29 21:24:14 +0300

    test

---
+++ museboard/js/museboard.js
@@ -0,0 +1,9 @@
+﻿console.log('museboard.js v1.01');
+function startInit(){
+	console.log('startInit');
+	console.log('done startInit');
+}
+window.onload = function () {
+	console.log('window.onload');
+	startInit();
+};
